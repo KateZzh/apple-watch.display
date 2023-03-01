6eed387f78a7e9c36fea1d318d7ef80c5e902053
KateZzh <ekaterin.pro2@gmail.com>
2023-03-02 00:23:55 +0300

    in developing

---
+++ app.js
@@ -0,0 +1,15 @@
+// const time = new Date();
+const time = new Date(2013, 0, 1, 0, 0, 0, 0);
+
+const dynamicTime = document.querySelector(".wrapper h1");
+const dynamicYear = document.querySelector(".wrapper p");
+
+// function doDynamicTime(time_, dynamicTime_) {
+//   dynamicTime_.innerHTML = ;
+// }
+// setInterval(doDynamicTime, 1000, time, dynamicTime);
+
+// function doDynamicYear(time_, dynamicYear_) {
+//   dynamicYear_.innerHTML = time_.getFullYear();
+// }
+// setInterval(doDynamicYear, 1000, time, dynamicYear);
