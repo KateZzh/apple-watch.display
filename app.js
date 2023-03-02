9c546f24f5a7d2df99e5f62ca1d84d081f778c11
KateZzh <ekaterin.pro2@gmail.com>
2023-03-02 22:45:11 +0300

    done

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-// const time = new Date();
-const time = new Date(2013, 0, 1, 0, 0, 0, 0);
-
 const dynamicTime = document.querySelector(".wrapper h1");
-const dynamicYear = document.querySelector(".wrapper p");
+const dynamicDate = document.querySelector(".wrapper p");
 
-// function doDynamicTime(time_, dynamicTime_) {
-//   dynamicTime_.innerHTML = ;
-// }
-// setInterval(doDynamicTime, 1000, time, dynamicTime);
+function doDynamicTime(dynamicTime_) {
+  const time = new Date();
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const clock = `${hours}:${minutes}:${seconds}`;
 
-// function doDynamicYear(time_, dynamicYear_) {
-//   dynamicYear_.innerHTML = time_.getFullYear();
-// }
-// setInterval(doDynamicYear, 1000, time, dynamicYear);
+  dynamicTime_.innerHTML = clock;
+}
+
+function doDynamicDate(dynamicDate_) {
+  const time = new Date().toLocaleDateString();
+
+  dynamicDate_.innerHTML = time;
+}
+
+setTimeout(doDynamicDate, 1000, dynamicDate);
+setInterval(doDynamicTime, 1000, dynamicTime);
