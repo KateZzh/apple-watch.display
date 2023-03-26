b1f181f3befdcd9f0488bb31674a1694f7b8d359
KateZzh <ekaterin.pro2@gmail.com>
2023-03-26 19:40:20 +0300

    some changes

--- app.js
+++ app.js
@@ -21,28 +21,56 @@
 // setInterval(doDynamicTime, 1000, dynamicTime);
 
 
+// class AppleWatchDisplay {
+//   constructor() {
+//     setTimeout(this.doDynamicDate, 1000, this.dynamicDate);
+//     setInterval(this.doDynamicTime, 1000, this.dynamicTime);
+//   }
+
+//   doDynamicTime() {
+//     const dynamicTime = document.querySelector(".wrapper h1");
+//     const time = new Date();
+//     const hours = time.getHours();
+//     const minutes = time.getMinutes();
+//     const seconds = time.getSeconds();
+//     const clock = `${hours}:${minutes}:${seconds}`;
+
+//     dynamicTime.innerHTML = clock;
+//   }
+
+//   doDynamicDate() {
+//     const dynamicDate = document.querySelector(".wrapper p");
+//     const time = new Date().toLocaleDateString();
+
+//     dynamicDate.innerHTML = time;
+//   }
+// }
+
+// const appleWatchDisplay = new AppleWatchDisplay();
+
 class AppleWatchDisplay {
   constructor() {
-    setTimeout(this.doDynamicDate, 1000, this.dynamicDate);
-    setInterval(this.doDynamicTime, 1000, this.dynamicTime);
+    setInterval(this.doDynamicDateTime, 1000);
   }
 
-  doDynamicTime() {
+  doDynamicDate() {
+    const dynamicDate = document.querySelector(".wrapper p");
+    const time = new Date().toLocaleDateString("ru-BY");
+
+    dynamicDate.innerHTML = time;
+  }
+
+  doDynamicDateTime() {
     const dynamicTime = document.querySelector(".wrapper h1");
+    const dynamicDate = document.querySelector(".wrapper p");
     const time = new Date();
     const hours = time.getHours();
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
     const clock = `${hours}:${minutes}:${seconds}`;
 
+    dynamicDate.innerHTML = time.toLocaleDateString("ru-BY");
     dynamicTime.innerHTML = clock;
-  }
-
-  doDynamicDate() {
-    const dynamicDate = document.querySelector(".wrapper p");
-    const time = new Date().toLocaleDateString();
-
-    dynamicDate.innerHTML = time;
   }
 }
 
